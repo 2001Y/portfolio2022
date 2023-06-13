8a28816137f88d12d947406c09f0fb8a3c279b6a
2001Y <15167896+2001Y@users.noreply.github.com>
2023-06-13 18:40:10 +0000

    add All

--- components/AdSence/AdSence.tsx
+++ components/AdSence/AdSence.tsx
@@ -4,31 +4,37 @@ import Script from 'next/script'
 
 export default function Output() {
 
-    const { asPath } = useRouter()
-    useEffect(() => {
-        try {
-            (window.adsbygoogle = window.adsbygoogle || []).push({})
-        } catch (e) {
-            console.error(e);
-        }
-    }, [asPath])
+  const { asPath } = useRouter()
+  useEffect(() => {
+    try {
+      (window.adsbygoogle = window.adsbygoogle || []).push({})
+    } catch (e) {
+      console.error(e);
+    }
+  }, [asPath])
 
-    return (
-        <>
-            <Script
-                src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-3750999099107987"
-                crossOrigin="anonymous"
-                async={true}
-                // strategy="afterInteractive"
-                onError={(e) => { console.error('Script failed to load', e) }}
-            />
-            <div key={asPath}>
-                <ins className="adsbygoogle"
-                    style={{ display: "block" }}
-                    data-ad-client="ca-pub-3750999099107987"
-                    data-ad-slot="7157516277"
-                    data-full-width-responsive="false" />
-            </div>
-        </>
-    );
+  return (
+    <>
+      <Script
+        src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-3750999099107987"
+        crossOrigin="anonymous"
+        async={true}
+        strategy="afterInteractive"
+        onError={(e) => { console.error('Script failed to load', e) }}
+      />
+      <div key={asPath} >
+        <ins className="adsbygoogle"
+          style={{ display: "block" }}
+          data-ad-client="ca-pub-3750999099107987"
+          data-ad-slot="7157516277"
+          data-full-width-responsive="false" />
+      </div>
+      <style jsx>{`
+        div {
+          width: 100%;
+          height: 100%;
+        }
+      `}</style>
+    </>
+  );
 }

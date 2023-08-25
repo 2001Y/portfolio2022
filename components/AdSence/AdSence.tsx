1fca1a824b5cde30108c626867d8ab4adbc68c49
2001Y <15167896+2001Y@users.noreply.github.com>
2023-08-25 08:12:14 +0000

    add all

--- components/AdSence/AdSence.tsx
+++ components/AdSence/AdSence.tsx
@@ -5,7 +5,7 @@ import Script from 'next/script'
 export default function Output() {
 
   const { asPath } = useRouter()
-  od() => {
+  useEffect(() => {
     try {
       (window.adsbygoogle = window.adsbygoogle || []).push({})
     } catch (e) {
